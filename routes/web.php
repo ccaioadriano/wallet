1deaf0559b0eaa40715111b3815380af1d32afab
ccaioadriano <caiobhadriano@gmail.com>
2024-12-25 15:00:30 -0300

    refactor: reorganizar rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\TransacaoController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ConfiguracaoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,19 +11,23 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/transacoes', [App\Http\Controllers\TransacaoController::class, 'index'])->name('transacao-index');
-Route::post('/transacoes', [App\Http\Controllers\TransacaoController::class, 'store'])->name('transacao-store');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/configuracoes', [App\Http\Controllers\ConfiguracaoController::class, 'index'])->name('configuracao-index');
-Route::post('/configuracoes/updateProfileImage', [App\Http\Controllers\ConfiguracaoController::class, 'updateProfileImage'])->name('atualiza-foto-perfil');
+// Rotas de Transações
+Route::prefix('transacoes')->name('transacao-')->group(function () {
+    Route::get('/', [TransacaoController::class, 'index'])->name('index');
+    Route::post('/', [TransacaoController::class, 'store'])->name('store');
+    Route::get('/{transacaoId}/edit', [TransacaoController::class, 'edit'])->name('edit');
+    Route::put('/{transacaoId}/update', [TransacaoController::class, 'update'])->name('update');
+    Route::delete('/{transacaoId}/delete', [TransacaoController::class, 'delete'])->name('delete');
+});
 
-Route::post('/adicionar-categoria', [App\Http\Controllers\ConfiguracaoController::class, 'addCategoria'])->name('adiciona-categoria');
+// Rotas de Configurações
+Route::prefix('configuracoes')->name('configuracao-')->group(function () {
+    Route::get('/', [ConfiguracaoController::class, 'index'])->name('index');
+    Route::post('/updateProfileImage', [ConfiguracaoController::class, 'updateProfileImage'])->name('atualiza-foto-perfil');
+    Route::post('/adicionar-categoria', [ConfiguracaoController::class, 'addCategoria'])->name('adiciona-categoria');
+});
 
-
-Route::get('/transacoes/{transacaoId}/edit', [TransacaoController::class, 'edit'])->name('transacao-edit');
-Route::put('/transacoes/{transacaoId}/update', [TransacaoController::class, 'update'])->name('transacao-update');
-Route::delete('/transacoes/{transacaoId}/delete', [TransacaoController::class, 'delete'])->name('transacao-delete');
-
-//rotas para js
-Route::post("recupera-informacao-grafico", [App\Http\Controllers\HomeController::class, "recuperaInformacaoGrafico"])->name('recupera-informacao-grafico');
+// Rotas para JS
+Route::post('recupera-informacao-grafico', [HomeController::class, 'recuperaInformacaoGrafico'])->name('recupera-informacao-grafico');
